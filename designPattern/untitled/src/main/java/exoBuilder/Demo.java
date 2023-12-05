d0394270d085e3b12339e528d0aa28f931eb42c7
Aurelien Boets <aurelien.boets59@gmail.com>
2023-12-05 10:08:58 +0100

    exoBuilder

--- designPattern/untitled/src/main/java/exoBuilder/Demo.java
+++ designPattern/untitled/src/main/java/exoBuilder/Demo.java
@@ -5,7 +5,14 @@ import exoBuilder.Enum.*;
 
 public class Demo {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza.Builder().size(PizzaSize.MEDIUM).typeOfDough(Dough.FINE).cheese(Cheese.MOZZARELLA).addTopping("Jambon").addTopping("Champignon").sauce(Sauce.TOMATO).build();
+        Pizza pizza = new Pizza.Builder()
+                .size(PizzaSize.MEDIUM)
+                .typeOfDough(Dough.FINE)
+                .cheese(Cheese.MOZZARELLA)
+                .addTopping("Jambon")
+                .addTopping("Champignon")
+                .sauce(Sauce.TOMATO)
+                .build();
         System.out.println(pizza);
     }
 }
